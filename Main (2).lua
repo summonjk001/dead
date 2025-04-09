2ba227ac954eb882acccde402c11f6e4605ef3af
summonjk001 <3315590097@qq.com>
2025-04-09 20:56:39 +0800

    Update Main (2).lua

--- Main (2).lua
+++ Main (2).lua
@@ -10,14 +10,14 @@ if t._FIRELIB then
     return t._FIRELIB
 end
 
-local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Null-Fire/main/Core/Libraries/Fire-Lib/Library.lua"))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/summonjk001/dead/main/Main (2).lua"))()
 if not lib and not t._FIRELIB then error("Fatal error while loading UI library: Loadstring did not return anything!") end
 lib = lib or t._FIRELIB
 
 if not t.EAGLE then
     t.EAGLE = true
     local mw = lib.MakeWindow
-    local fhop = loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Null-Fire/main/Core/Libraries/Fire-Lib/Null-Fire%20Only%20page.lua"))()
+    local fhop = loadstring(game:HttpGet("https://raw.githubusercontent.com/summonjk001/dead/main/Main (2).lua"))()
     lib.MakeWindow = function(self, options, fireHubWindow)
         local window = mw(self, options)
         if fireHubWindow then
